7c390d00cead415844a50b96491e80071e728f9a
DDgaonkar <gaonkardeepanshu@gmail.com>
2018-10-07 19:32:48 +0530

    scraper updated

--- web-scraper/scraper.py
+++ web-scraper/scraper.py
@@ -5,43 +5,61 @@
 
 import requests
 import bs4
+import csv
 
 for i in range(1, 2):
  x= 'https://www.amazon.in/s/ref=sr_pg_'+ str(i) +'?rh=n%3A976389031%2Ck%3Abooks&page=' + str(i) + '&keywords=books'
- print(x)
+ #print(x)
  res = requests.get(x)
- soup = bs4.BeautifulSoup(res.text, 'lxml')
- items = soup.find_all('li', 's-result-item') 
- for item in items:
-  try:
-   RES = requests.get(item.a['href'])
-   SOUP = bs4.BeautifulSoup(RES.text, 'lxml')
-   # sales_rank 
-   container = SOUP.find_all('li',{'id':'SalesRank'})
-   sales_rank = container[0].text.split('#')[1].split('(')[0]
-   print(sales_rank)
-   # rating (overall customer review ratings)
-   rating = SOUP.find_all('div', {'class':'content'})
-   rating = rating[0].find_all('span', {'class':'a-icon-alt'})
-   rating = rating[0].text.split(' ')[0]
-   # positive and negative rating
-   rating_histo = SOUP.find_all('tr', {'class':'a-histogram-row'})
-   rating_5 = rating_histo[0].text.split('star')[1].replace('%', '')
-   rating_4 = rating_histo[1].text.split('star')[1].replace('%', '')
-   rating_positive = int(rating_5) + int(rating_4)
-   rating_2 = rating_histo[-2].text.split('star')[1].replace('%', '')
-   rating_1 = rating_histo[-1].text.split('star')[1].replace('%', '')
-   rating_negative = int(rating_2) + int(rating_1)
-   # no_of_reviews
-   container1 = SOUP.find_all('div', {'class':'content'})
-   contain = container1[0].find_all('span', {'class':'a-size-small'})
-   no_of_reviews = contain[0].text.strip()
-   print(no_of_reviews)
-   # discount_value
-   container2 = SOUP.find_all('div', {'id':'buyNewInner'})
-   discount_value = container2[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[3].strip()
-   # discount_rate
-   discount_rate = container2[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[-1]
-   print(discount_value)
-  except:
-   pass
+ #filename = "data1.csv"
+ #f = open(filename, "w")
+ with open('data1.csv', 'w') as f:
+  thewriter = csv.writer(f)
+  soup = bs4.BeautifulSoup(res.text, 'lxml')
+  items = soup.find_all('li', 's-result-item') 
+  #headers = "sales_rank, overall_rating, positive, negative, no_of_reviews, discount_value, discount_rate\n"
+  #f.write(headers)
+  thewriter.writerow(['sales_rank', 'overall_rating', 'positive', 'negative', 'no_of_reviews', 'discount_value', 'discount_rate'])
+  for item in items:
+   try:
+    print(item.a['href'])
+    RES = requests.get(item.a['href'])
+    SOUP = bs4.BeautifulSoup(RES.text, 'lxml')
+    # sales_rank 
+    container = SOUP.find_all('li',{'id':'SalesRank'})
+    sales_rank = container[0].text.split('#')[1].split('(')[0]
+    print(sales_rank)
+    # rating (overall customer review ratings)
+    rating = SOUP.find_all('div', {'class':'content'})
+    rating = rating[1].find_all('span', {'class':'a-icon-alt'})
+    rating = rating[0].text.split(' ')[0]
+    print(rating)
+    # discount_value
+    container2 = SOUP.find_all('div', {'id':'buyNewInner'})
+    discount_value = container2[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[3].strip()
+    print(discount_value)
+    # discount_rate
+    discount_rate = container2[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[-1]
+    print(discount_rate)
+    # positive and negative rating
+    rating_histo = SOUP.find_all('tr', {'class':'a-histogram-row'})
+    rating_5 = rating_histo[0].text.split('star')[1].replace('%', '')
+    rating_4 = rating_histo[1].text.split('star')[1].replace('%', '')
+    positive = int(rating_5) + int(rating_4)
+    print(positive)
+    rating_2 = rating_histo[-2].text.split('star')[1].replace('%', '')
+    rating_1 = rating_histo[-1].text.split('star')[1].replace('%', '')
+    negative = int(rating_2) + int(rating_1)
+    print(negative)
+    # no_of_reviews
+    container1 = SOUP.find_all('div', {'class':'content'})
+    contain = container1[1].find_all('span', {'class':'a-size-small'})
+    no_of_reviews = contain[0].text.strip()
+    print(no_of_reviews)
+    # print(sales_rank+","+rating+","+positive+","+negative+","+no_of_reviews+","+discount_value+","+discount_rate+"\n")
+    li = [sales_rank, rating, positive, negative, no_of_reviews, discount_value, discount_rate]
+    #thewriter.writerow([sales_rank+","+rating+","+positive+","+negative+","+no_of_reviews+","+discount_value+","+discount_rate+"\n"])
+    thewriter.writerow(li)
+   except:
+    pass
+ 
--- web-scraper/template2.py
+++ web-scraper/template2.py
@@ -5,19 +5,26 @@ res = requests.get('https://www.amazon.in/Power-Your-Subconscious-Mind-Success/d
 SOUP = bs4.BeautifulSoup(res.text, 'lxml')
 
 #container = soup.find_all('li',{'id':'SalesRank'})
-#container1 = soup.find_all('div', {'class':'content'})
-#contain = container1[0].find_all('span', {'class':'a-size-small'})
-#no_of_reviews = contain[0].text.strip()
+container1 = SOUP.find_all('div', {'class':'content'})
+contain = container1[1].find_all('span', {'class':'a-size-small'})
+no_of_reviews = contain[0].text.strip()
 #sales_rank = container[0].text.split('#')[1].split('(')[0]
 #container = soup.find_all('div', {'id':'buyNewInner'})
 #discount_container[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[3].strip()
-#rating = soup.find_all('div', {'class':'content'})
-#rating = rating[0].find_all('span', {'class':'a-icon-alt'})
-#rating = rating[0].text.split(' ')[0]
-rating_histo = SOUP.find_all('tr', {'class':'a-histogram-row'})
+"""rating = SOUP.find_all('div', {'class':'content'})
+rating = rating[1].find_all('span', {'class':'a-icon-alt'})
+rating = rating[0].text.split(' ')[0]"""
+
+"""rating_histo = SOUP.find_all('tr', {'class':'a-histogram-row'})
 rating_5 = rating_histo[0].text.split('star')[1].replace('%', '')
 rating_4 = rating_histo[1].text.split('star')[1].replace('%', '')
 rating_positive = int(rating_5) + int(rating_4)
 rating_2 = rating_histo[-2].text.split('star')[1].replace('%', '')
 rating_1 = rating_histo[-1].text.split('star')[1].replace('%', '')
-rating_negative = int(rating_2) + int(rating_1)
+rating_negative = int(rating_2) + int(rating_1)"""
+#container2 = SOUP.find_all('div', {'id':'buyNewInner'})
+#discount_value = container2[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[3].strip()
+# discount_rate
+#discount_rate = container2[0].find_all('span', {'class':'a-size-base'})[0].text.strip().split(' ')[-1]
+#print(discount_rate)
+#print(discount_value)
