a7d04caaa2446c3c757c26686939b998242b7572
DDgaonkar <gaonkardeepanshu@gmail.com>
2019-01-27 19:59:01 +0530

    feature selecion added

--- Neural Network/NN.py
+++ Neural Network/NN.py
@@ -25,11 +25,13 @@ Y  = dataset.iloc[:,0].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
+"""
 # feature scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+"""
 
 # Importing keras libraries and packages
 import keras 
@@ -41,29 +43,30 @@ model = Sequential()
 
 # Adding the input layer and the first hidden layer
 # model.add(Dense(4, kernel_initializer = 'uniform', activation = 'relu', input_dim == 6))    
-model.add(Dense(4, kernel_initializer = 'uniform' , activation = 'linear', input_dim = 6))    
+model.add(Dense(4, kernel_initializer = 'uniform' , activation = 'relu', input_dim = 6))    
 
 # Adding another hidden layer 
-model.add(Dense(4, kernel_initializer = 'uniform', activation = 'linear'))
+model.add(Dense(4, kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding the output layer 
 model.add(Dense(units= 1, kernel_initializer = 'uniform', activation = 'linear'))        
 
 # Compiling the ANN
-model.compile(optimizer = 'adam', loss = 'mse', metrics = ['accuracy'])
+model.compile(optimizer = 'rmsprop', loss = 'mse', metrics = ['accuracy'])
 
 # Fitting the ANN to the training set 
-model.fit(X_train, Y_train, batch_size = 10, epochs = 100)
+model.fit(X_train, Y_train, batch_size = 10, epochs = 100, validation_split=0.2)
 
 # predicting the test set result 
 Y_pred = model.predict(X_test)
 
+"""
 # Applying k-fold cross validation 
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = model, X = X_train, y = Y_train, cv = 10)
 accuracies.mean()
 accuracies.std()
-
+"""
 """
 # Making the confusion matrix 
 
